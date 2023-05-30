8bd21f679bbf5550f6ae08ea467df8ef80452f80
swapnil <swapnilsinhsodha@gmail.com>
2023-05-30 07:43:25 +0530

    commit in new-branch

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,23 @@ router.post('/', function(req, res, next) {
   }
 });
 
+router.post('/', function(req, res, next) {
+  try{
+    const {message} = req.body
+    res.json({
+      type:"error",
+      data:message,
+      status:500
+    })
+  }
+  catch(error){
+    console.log(error);
+    res.json({
+      type:"error",
+      datat:"",
+      status:500
+    })
+  }
+});
+
 module.exports = router;
